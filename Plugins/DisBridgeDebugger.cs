4030624f904b031a8a70ba09026bd1ccc324d93b
Child of the Beast <dragon_5556@msn.com>
2022-06-30 21:34:18 -0400

    Added plugins

---
+++ Plugins/DisBridgeDebugger.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+using UdonVR.DisBridge;
+using TMPro;
+
+namespace UdonVR.DisBridge.Plugins
+{
+    [UdonBehaviourSyncMode(BehaviourSyncMode.None)]
+    public class DisBridgeDebugger : UdonSharpBehaviour
+    {
+        [SerializeField] private PluginManager manager;
+
+        [SerializeField] private TextMeshProUGUI debugUi;
+
+        private void Start()
+        {
+            manager.debugger = this;
+        }
+
+        public void log(string _log)
+        {
+            debugUi.text = _log;
+        }
+    }
+}
---
+++ Plugins/RoleBoard.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using TMPro;
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+using VRCPrefabs.CyanEmu;
+
+namespace UdonVR.DisBridge.Plugins
+{
+    [UdonBehaviourSyncMode(BehaviourSyncMode.None)]
+    public class RoleBoard : UdonSharpBehaviour
+    {
+        [SerializeField] private PluginManager manager;
+
+        public int roleContainer = 0;
+        public TextMeshProUGUI roleTitle;
+        public TextMeshProUGUI roleList;
+        public string seperator = "</color> | <#3498db>";
+        
+        private void Start()
+        {
+            manager.log($"Registering {nameof(RoleBoard)}:{gameObject.name}...");
+            manager.AddPlugin(gameObject);
+        }
+        
+        //Runs when DisBridge finishes pulling the roles.
+        public void _Init()
+        {
+            manager.log($"_Init {nameof(RoleBoard)}:{gameObject.name}...");
+            string _roleList = String.Join(seperator, manager.GetRole(roleContainer).GetMembers());
+            roleTitle.text = roleTitle.text.Replace("{roleTitle}",manager.GetRole(roleContainer).roleName);
+            roleList.text = roleList.text.Replace("{roleList}",_roleList);
+        }
+    }
+}
