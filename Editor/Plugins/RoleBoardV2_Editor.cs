156d9e287d7deffc8727bd7bdbb2e3bedae9a37c
SelleSensen <148486261+SelleSensen@users.noreply.github.com>
2023-10-23 14:50:22 +0200

    Update RoleBoardV2_Editor.cs
    Updated the editor script to reflect the new RoleBoardV2 functionality

--- Editor/Plugins/RoleBoardV2_Editor.cs
+++ Editor/Plugins/RoleBoardV2_Editor.cs
@@ -1,4 +1,4 @@
-﻿//Copyright (c) 2023 UdonVR LLC - All Rights Reserved
+//Copyright (c) 2023 UdonVR LLC - All Rights Reserved
 using System;
 using System.Collections;
 using System.Collections.Generic;
@@ -34,13 +34,6 @@ namespace UdonVR.DisBridge.RoleBoardV2
             if (myScript._mode == 2) EditorFunctions.DrawRoleContainerSelector(myScript);
             EditorFunctions.GuiLine();
             
-            float _publicdDisplayTime = EditorGUILayout.FloatField("Display Time: ",myScript.displayTime);
-            if (_publicdDisplayTime != myScript.displayTime)
-            {
-                Undo.RecordObject(myScript,$"Changed displayTime: {myScript.displayTime} => {_publicdDisplayTime}");
-                myScript.displayTime = _publicdDisplayTime;
-            }
-            
             float _publicFadeSpeed = EditorGUILayout.FloatField("Fade Speed: ",myScript.publicFadeSpeed);
             if (_publicFadeSpeed != myScript.publicFadeSpeed)
             {
@@ -49,11 +42,50 @@ namespace UdonVR.DisBridge.RoleBoardV2
                 myScript.fadeSpeed = _publicFadeSpeed * .5f;
             }
 
+            float _scrollSpeed = EditorGUILayout.FloatField("Scroll speed: ", myScript.scrollSpeed);
+            if (_scrollSpeed != myScript.scrollSpeed)
+            {
+                Undo.RecordObject(myScript, $"Changed scrollSpeed: {myScript.scrollSpeed} => {_scrollSpeed}");
+                myScript.scrollSpeed = _scrollSpeed;
+            }
+
+            float _scrollStopPointRelative = EditorGUILayout.FloatField("Scroll stop point: ", myScript.scrollStopPointRelative);
+            if (_scrollStopPointRelative != myScript.scrollStopPointRelative)
+            {
+                Undo.RecordObject(myScript, $"Changed scrollStopPointRelative: {myScript.scrollStopPointRelative} => {_scrollStopPointRelative}");
+                myScript.scrollStopPointRelative = _scrollStopPointRelative;
+            }
+
+            float _waitTimeBeforeScroll = EditorGUILayout.FloatField("Wait time before scroll: ", myScript.waitTimeBeforeScroll);
+            if (_waitTimeBeforeScroll != myScript.waitTimeBeforeScroll)
+            {
+                Undo.RecordObject(myScript, $"Changed waitTimeBeforeScroll: {myScript.waitTimeBeforeScroll} => {_waitTimeBeforeScroll}");
+                myScript.waitTimeBeforeScroll = _waitTimeBeforeScroll;
+            }
+
+            float _waitTimeAfterScroll = EditorGUILayout.FloatField("Wait time after scroll: ", myScript.waitTimeAfterScroll);
+            if (_waitTimeAfterScroll != myScript.waitTimeAfterScroll)
+            {
+                Undo.RecordObject(myScript, $"Changed waitTimeAfterScroll: {myScript.waitTimeAfterScroll} => {_waitTimeAfterScroll}");
+                myScript.waitTimeAfterScroll = _waitTimeAfterScroll;
+            }
+
+            float _manualScrollingWaitTime = EditorGUILayout.FloatField("Wait time after manual scroll: ", myScript.manualScrollingWaitTime);
+            if (_manualScrollingWaitTime != myScript.manualScrollingWaitTime)
+            {
+                Undo.RecordObject(myScript, $"Changed manualScrollingWaitTime: {myScript.manualScrollingWaitTime} => {_manualScrollingWaitTime}");
+                myScript.manualScrollingWaitTime = _manualScrollingWaitTime;
+            }
+
+
+
+
+
             DrawDebug(myScript);
             PrefabUtility.RecordPrefabInstancePropertyModifications(myScript);
         }
 
-        private void DrawDebug(DisBridgePlugin _script)
+        private void DrawDebug(RoleBoardV2 _script)
         {
             bool _isDebug = EditorGUILayout.Toggle(_script.isDebug);
             if (_isDebug != _script.isDebug)
